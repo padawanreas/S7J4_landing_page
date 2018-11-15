b8d7b84a5ee45591ec47bb78cf046fa5a2a0e262
Maxime-Beaufils <thelimier@gmail.com>
2018-11-15 15:29:51 +0100

    upload app web

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/index'
-  get 'static_pages/lp2'
-  get 'static_pages/lp3'
+  get "/lp1", to: 'static_pages#index'
+  get "/lp2", to: 'static_pages#lp2'
+  get "/lp3", to: 'static_pages#lp3'
   root 'static_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
